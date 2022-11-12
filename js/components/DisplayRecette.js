2999fe1dd8f427a45f8ab8b102f36fdb28fa8638
Olivier Bussier <olivier@bussier.fr>
2022-11-12 08:09:02 +0100

    Choix images local distant

--- js/components/DisplayRecette.js
+++ js/components/DisplayRecette.js
@@ -32,9 +32,13 @@ export class DisplayRecette {
         } else {
             debugStr = ''
         }
-        // const stfl = 'abcdefghijklmnopqrstuvwxyz0123456789'
-        // const img = "assets/images/new/" + String.fromCharCode(stfl.charCodeAt(Math.floor(Math.random()*stfl.length))) + ".jpg"
-        const img = "https://picsum.photos/380/178?a=" + Math.random(100000)
+        var img
+        if (1) {
+            const stfl = 'abcdefghijklmnopqrstuvwxyz0123456789'
+            img = "assets/images/new/" + String.fromCharCode(stfl.charCodeAt(Math.floor(Math.random()*stfl.length))) + ".jpg"
+        } else {
+            img = "https://picsum.photos/380/178?a=" + Math.random(100000)
+        }
 
         section.innerHTML =
            `<article class="card">
